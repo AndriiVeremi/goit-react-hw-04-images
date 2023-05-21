7641e0cd344b04e87be0b3c78fbd1bf5187ecf84
Andrii Veremii <dashuk10@gmail.com>
2023-05-21 20:59:46 +0300

    add

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -4,17 +4,14 @@ import PropTypes from 'prop-types';
 import { ReactComponent as Search } from '../../icons/search.svg';
 import { Header, Form, Button, Span, Input } from './SearchBar.styled';
 
-function SearchBar({onSubmit}) {
+function SearchBar({ onSubmit }) {
   const [value, setValue] = useState('');
-  // state = {
-  //   value: '',
-  // };
 
   const handelSearch = event => {
     setValue(event.currentTarget.value.toLowerCase());
   };
 
-   const handelSubmit = event => {
+  const handelSubmit = event => {
     event.preventDefault();
     if (value.trim() === '') {
       return toast.error('Nothing was entered');
@@ -23,24 +20,24 @@ function SearchBar({onSubmit}) {
     setValue('');
   };
 
-    return (
-      <Header>
-        <Form onSubmit={handelSubmit}>
-          <Button type="submit">
-            <Search />
-            <Span>Search</Span>
-          </Button>
+  return (
+    <Header>
+      <Form onSubmit={handelSubmit}>
+        <Button type="submit">
+          <Search />
+          <Span>Search</Span>
+        </Button>
 
-          <Input
-            type="text"
-            name="value"
-            value={value}
-            onChange={handelSearch}
-            placeholder="Search images and photos"
-          />
-        </Form>
-      </Header>
-    );
+        <Input
+          type="text"
+          name="value"
+          value={value}
+          onChange={handelSearch}
+          placeholder="Search images and photos"
+        />
+      </Form>
+    </Header>
+  );
 }
 
 export default SearchBar;
@@ -48,63 +45,3 @@ export default SearchBar;
 SearchBar.propType = {
   onSubmit: PropTypes.func.isRequired,
 };
-
-
-
-// import React, { Component } from 'react';
-// import { toast } from 'react-toastify';
-// import PropTypes from 'prop-types';
-// import { ReactComponent as Search } from '../../icons/search.svg';
-// import { Header, Form, Button, Span, Input } from './SearchBar.styled';
-
-// class SearchBar extends Component {
-//   state = {
-//     value: '',
-//   };
-
-//   handelSearch = event => {
-//     this.setState({ value: event.currentTarget.value.toLowerCase() });
-//   };
-
-//   handelSubmit = event => {
-//     event.preventDefault();
-//     if (this.state.value.trim() === '') {
-//       return toast.error('Nothing was entered');
-//     }
-//     this.props.onSubmit(this.state.value);
-//     this.resetForm();
-//   };
-
-//   resetForm = () => {
-//     this.setState({
-//       value: '',
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <Header>
-//         <Form onSubmit={this.handelSubmit}>
-//           <Button type="submit">
-//             <Search />
-//             <Span>Search</Span>
-//           </Button>
-
-//           <Input
-//             type="text"
-//             name="value"
-//             value={this.state.value}
-//             onChange={this.handelSearch}
-//             placeholder="Search images and photos"
-//           />
-//         </Form>
-//       </Header>
-//     );
-//   }
-// }
-
-// export default SearchBar;
-
-// SearchBar.propType = {
-//   onSubmit: PropTypes.func.isRequired,
-// };
